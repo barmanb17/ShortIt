76cb0f375120b963dfa8b401b4fb7a51b0295685
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-11 01:36:14 +0530

    implementing express

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ import { createServer } from 'http';
 import crypto from "crypto";
 import path from 'path';
 import { writeFile } from 'fs/promises';
+import express from "express";
+
+const app = express();
 
 
 const PORT = 3000;
 const DATA_FILE = path.join("data", "links.json");
+
+app.use(express.static("public"));
 
 const serveFile = async (res, filePath, contentType) => {
     try {
@@ -37,6 +42,32 @@ const saveLinks = async (links) =>{
     await writeFile(DATA_FILE, JSON.stringify(links));
 }
 
+app.get("/", async(req, res) => {
+    try {
+        const file = await fstat.readFile(path.join("views", "index.html"));
+        const links = await loadLinks();
+
+        const content = file.toString().replaceAll("{{shortened_urls}}", )
+    } catch (error) {
+        console.error(err);
+        return res.status(500).send("internal server error");
+    }
+})
+
+app.post("/", async (req, res) => {
+    try {
+        const {url, shortCode} = req.body;
+        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+        if(links[finalShortCode]) {
+            return res.send(400).send("Short code already exists. Please choose another.");
+        }
+            links[finalShortCode] = url;
+            await saveLinks(links);
+    } catch (error) {
+        
+    }
+})
+
 const server = createServer(async (req, res) => {
     if (req.method === "GET") {
         if (req.url === "/") {
@@ -64,32 +95,7 @@ const server = createServer(async (req, res) => {
         }
     }
 
-    if (req.method === "POST" && req.url === "/shorten") {
-        const links = await loadLinks();
-        let body = "";
-        req.on("data", (chunk) => (body += chunk));
-        req.on("end", async () => {
-            console.log(body);
-            const { url, shortCode } = JSON.parse(body);
-            if (!url) {
-                res.writeHead(400, { "Content-Type": "text/plain" });
-                return res.end("URL is required");
-            }
 
-            const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-            if (links[finalShortCode]) {
-                res.writeHead(400, { "Content-Type": "text/plain" });
-                return res.end("Short code already exists. Please choose another.");
-            }
-
-            links[finalShortCode] = url;
-            await saveLinks(links);
-
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
-        });
-        return; // ✅ Prevents falling through to 404
-    }
 
     // ✅ Only run 404 if no valid route matched
     res.writeHead(404, { "Content-Type": "text/plain" });
